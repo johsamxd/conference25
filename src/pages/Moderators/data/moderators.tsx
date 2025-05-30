9d7d8a459ede8b1ed608b2a93b3ddcb905c481b4
Aman work-pc <a.kyrdzhagasov@innopolis.ru>
2025-05-30 15:33:13 +0900

    moderators enchancements

--- src/pages/Moderators/data/moderators.tsx
+++ src/pages/Moderators/data/moderators.tsx
@@ -2,6 +2,7 @@ export const MODERATORS = [
   {
     id: "moderator1",
     name: "Федорова Анджела Иннокентьевна",
+    tag: "44-ФЗ",
     image: "/images/moderators/fedorova.jpg",
     short_description:
       "начальник отдела организации закупок на оказание услуг и поставку товаров",
@@ -9,6 +10,7 @@ export const MODERATORS = [
   {
     id: "moderator2",
     name: "Евсеева Марина Прокопьевна",
+    tag: "223-ФЗ",
     image: "/images/moderators/evseeva.jpg",
     short_description:
       "начальник отдела по работе с отдельными юридическими лицами",
--- src/pages/Moderators/index.tsx
+++ src/pages/Moderators/index.tsx
@@ -5,14 +5,21 @@ export function Moderators() {
   return (
     <div id="moderators" className="flex flex-col gap-10  px-4 pt-4">
       <h2 className="w-full text-center text-text-primary">Модераторы</h2>
-      <div className="p-5 rounded-2xl grid grid-cols-1 sm:grid-cols-2  gap-5 ">
+      <div className="p-5 rounded-2xl grid grid-cols-1 sm:grid-cols-2 gap-5">
         {MODERATORS.map((m, i) => (
           <div
             key={i}
-            className="grid grid-cols-3 bg-background-primary/90 rounded-xl shadow-white"
+            className="flex flex-col md:grid md:grid-cols-3 bg-background-primary/90 rounded-xl shadow-white"
           >
-            <div className="col-span-1">
-              <img src={m.image} className="rounded-l-xl" />
+            <div className="relative col-span-1 h-[46vh]">
+              <img
+                src={m.image}
+                className="w-full rounded-t-xl md:rounded-tr-none md:rounded-l-xl object-cover h-full"
+              />
+              <div className="absolute top-2 left-2 bg-main-secondary/70 text-white rounded-md px-2 py-1 text-sm font-medium">
+                {/* Здесь вы можете вставить текст вашего тега */}
+                {m.tag}
+              </div>
             </div>
             <div className="col-span-2 p-4 text-text-secondary flex flex-col gap-2.5">
               <h3>{m.name}</h3>
@@ -22,7 +29,7 @@ export function Moderators() {
               </div>
               <div className="flex gap-1 items-center">
                 <Icon type="calendar" size="md" color="dark" />
-                {"*ссылка?*"}
+                {"*ссылка*"}
               </div>
               <span>{m.short_description}</span>
             </div>
