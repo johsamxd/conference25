7ceb052b813ccfaf00977af20b94062f8cd76c37
Aman work-pc <a.kyrdzhagasov@innopolis.ru>
2025-06-02 10:24:46 +0900

    partners links

--- src/blocks/Partners/index.tsx
+++ src/blocks/Partners/index.tsx
@@ -8,18 +8,23 @@ import Autoplay from "embla-carousel-autoplay";
 const PARTNERS = [
   {
     image: "/images/partners/etp_gpb.png",
+    link: "https://etpgpb.ru/",
   },
   {
     image: "/images/partners/fabrikant.png",
+    link: "https://fabrikant.ru/",
   },
   {
     image: "/images/partners/roseltorg.jpg",
+    link: "https://roseltorg.ru/",
   },
   {
     image: "/images/partners/rts_tender.svg",
+    link: "https://rts-tender.ru/",
   },
   {
     image: "/images/partners/tek_torg.svg",
+    link: "https://tektorg.ru/",
   },
 ];
 
@@ -38,9 +43,14 @@ export function Partners() {
       <CarouselContent className="flex justify-between items-center mt-10">
         {PARTNERS?.map((p, i) => (
           <CarouselItem key={i} className="md:basis-1/2 lg:basis-1/3">
-            <div className="flex justify-center h-[10vh]">
+            <a
+              href={p.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex justify-center h-[10vh]"
+            >
               <img src={p.image} className="h-full" />
-            </div>
+            </a>
           </CarouselItem>
         ))}
       </CarouselContent>
