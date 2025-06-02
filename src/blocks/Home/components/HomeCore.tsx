80e3c09704b9a4ee08ced4f6caeba9d7667d5799
Aman work-pc <a.kyrdzhagasov@innopolis.ru>
2025-06-02 10:39:20 +0900

    home core links

--- src/blocks/Home/components/HomeCore.tsx
+++ src/blocks/Home/components/HomeCore.tsx
@@ -3,6 +3,7 @@ import SplitText from "@/components/animated/SplitText";
 import { Typewriter } from "@/components/animated/Typewriter";
 import { CountdownCalendar } from "@/components/CountdownCalendar";
 import { Button } from "@/components/ui/button";
+import { Icon } from "@/components/ui/icon";
 
 export function HomeCore() {
   return (
@@ -27,38 +28,46 @@ export function HomeCore() {
         </div>
       </div>
       <div className="flex flex-col gap-10 lg:w-[50vw] lg:items-center">
-        <div className="flex flex-col  gap-4">
+        <div className="flex flex-col gap-4 w-full">
           <div className="flex justify-center">
             <CountdownCalendar targetDate={new Date(2025, 5, 27, 9, 0, 0)} />
           </div>
 
-          <div className="flex flex-col gap-2.5">
+          <div className="flex flex-col gap-2.5 w-full">
             <h3>Дата:</h3>
             <div className="flex gap-2.5">
               <DateContent /> <SplitText text="июня 2025 года" type="h2" />
             </div>
           </div>
-          <div className="flex flex-col gap-2.5">
+          <div className="flex flex-col gap-2.5 w-full">
             <h3>Место проведения:</h3>
             <a
               href="https://2gis.ru/yakutsk/inside/7037510072941119"
               target="_blank"
               rel="noopener noreferrer"
+              className="flex items-center gap-3 group hover:pl-2 duration-300"
             >
               <SplitText
                 text="Дом дружбы народов им. А.Е. Кулаковского"
                 type="h2"
               />
+              <div className="invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity duration-300">
+                <Icon type="arrow-right" size="lg" color="white" />
+              </div>
             </a>
           </div>
-          <div className="flex flex-col gap-2.5">
+          <div className="flex flex-col gap-2.5 w-full">
             <h3>Адрес:</h3>
             <a
               href="https://2gis.ru/yakutsk/inside/7037510072941119"
               target="_blank"
               rel="noopener noreferrer"
+              className="flex items-center gap-3 group hover:pl-2 duration-300"
             >
               <SplitText text="г. Якутск, улица Пояркова, 4" type="h2" />
+              <div className="invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity duration-300">
+                <Icon type="arrow-right" size="lg" color="white" />
+              </div>
             </a>
           </div>
           <div className="flex justify-center items-center">
