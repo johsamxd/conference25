d039428e4e58fab24a090f641875f6a691ac7550
Aman work-pc <a.kyrdzhagasov@innopolis.ru>
2025-06-05 17:18:22 +0900

    removed tag

--- src/blocks/Moderators/index.tsx
+++ src/blocks/Moderators/index.tsx
@@ -12,14 +12,11 @@ export function Moderators() {
             key={i}
             className="flex flex-col md:grid md:grid-cols-3 bg-background-primary/90 rounded-xl shadow-white"
           >
-            <div className="relative col-span-1 h-[46vh]">
+            <div className="col-span-1 h-[46vh]">
               <img
                 src={m.image}
                 className="w-full rounded-t-xl md:rounded-tr-none md:rounded-l-xl object-cover h-full"
               />
-              <div className="absolute top-2 left-2 bg-main-secondary/70 text-white rounded-md px-2 py-1 text-sm font-medium">
-                {m.tag}
-              </div>
             </div>
             <div className="col-span-2 p-4 text-text-secondary flex flex-col gap-2.5">
               <h3>{m.name}</h3>
