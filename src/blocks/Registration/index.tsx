e1978aad3f9c6057f477a7b371a0406e8af2531b
Aman work-pc <a.kyrdzhagasov@innopolis.ru>
2025-06-05 17:32:49 +0900

    registration paddings

--- src/blocks/Registration/index.tsx
+++ src/blocks/Registration/index.tsx
@@ -12,7 +12,7 @@ export function Registration() {
           Регистрация на конференцию
         </h2>
         <div className="flex flex-col  md:flex-row">
-          <div className="flex flex-col gap-5 p-2.5 border-r-2 justify-center">
+          <div className="flex flex-col gap-5 py-2 px-4 border-r-2 justify-center">
             <CountdownCalendar targetDate={new Date(2025, 5, 27, 9, 0, 0)} />
             <div className="flex flex-col gap-2.5 items-center text-center">
               <p className="typo-sub-title text-text-alternative">
@@ -22,7 +22,7 @@ export function Registration() {
             </div>
           </div>
 
-          <div className="flex flex-col gap-4 p-2.5">
+          <div className="flex flex-col gap-4 py-2 px-4">
             <div className="flex flex-col gap-2.5 w-full">
               <h3 className="typo-sub-title">Дата:</h3>
               <div className="flex gap-2.5">
@@ -72,7 +72,7 @@ export function Registration() {
             </div>
 
             <a href="#" id="timepad_twf_register_3404754" className="w-full">
-              <Button className="rounded-lg w-full mt-2.5">
+              <Button className="rounded-lg w-full mb-2.5">
                 Пройти регистрацию
               </Button>
             </a>
