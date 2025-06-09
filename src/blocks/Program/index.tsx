affe4b14712ce528474d75f61b205c866011eaea
Aman work-pc <a.kyrdzhagasov@innopolis.ru>
2025-06-09 09:53:42 +0900

    full width programm block

--- src/blocks/Program/index.tsx
+++ src/blocks/Program/index.tsx
@@ -1,4 +1,3 @@
-import { cn } from "@/lib/utils";
 import { ProgramBlock } from "./components/ProgramBlock";
 import { PROGRAM } from "./data/program";
 
@@ -8,18 +7,7 @@ export function Program() {
       <div className="flex flex-col gap-10">
         <h2 className="w-full text-center text-text-primary pt-5">Программа</h2>
         {PROGRAM.map((p, i) => (
-          <div
-            key={i}
-            className={cn(
-              "flex flex-row-reverse items-center",
-              i % 2 && "flex-row"
-            )}
-          >
-            <ProgramBlock data={p} />
-            <div className="w-1/3 hidden lg:flex justify-center">
-              <span className="text-9xl font-medium text-center">{i + 1}</span>
-            </div>
-          </div>
+          <ProgramBlock key={i} data={p} />
         ))}
       </div>
     </div>
